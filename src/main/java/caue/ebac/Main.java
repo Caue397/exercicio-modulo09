0c4a6210bb8329beabe42ed71f02191979a23ba0
Cauê Gallizzi <cauegallizzi397@gmail.com>
2024-09-24 15:33:33 -0300

    finalizado

---
+++ src/main/java/caue/ebac/Main.java
@@ -0,0 +1,12 @@
+package caue.ebac;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        long numero = 10000000;
+        System.out.println(numero);
+        Long numero2 = numero;
+        System.out.println(numero2);
+    }
+}
